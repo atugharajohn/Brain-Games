b07c2bffd574b61205f5b3ca0c2e8b0cec6026fd
atugharajohn <atugharajohn@gmail.com>
2023-01-28 19:51:51 +0300

    Update files

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,16 @@
+from random import randint
+import brain_games.game_logic
+
+def find_gcd():
+    number1 = randint(0, 100)
+    number2 = randint(0, 100)
+    question = '{} {}'.format(number1, number2)
+   
+    while number1 != 0 and number2 != 0:
+        if number1 > number2:
+            number1 = number1 % number2
+        else:
+            number2 = number2 % number1
+    correct_answer = str(number1 + number2)
+
+    brain_games.game_logic.user_answers(question, correct_answer)
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import brain_games.game_logic
+import brain_games.games.gcd
+
+def main():
+    brain_games.game_logic.welcome_user()
+    print('Find the greatest common divisor of given numbers.')
+    brain_games.games.gcd.find_gcd()
+    while brain_games.game_logic.current_score < 3 \
+        and brain_games.game_logic.current_score \
+            != brain_games.game_logic.last_score:
+        brain_games.games.gcd.find_gcd()
+
+
+if __name__ == '__main__':
+    main()
